5531409e3845ff1c1acb8a340706c604aefc9daa
woodjessem <jwood6@g.clemson.edu>
2017-08-17 12:43:10 -0400

    8/17 work

--- practice_piwa_170817.R
+++ practice_piwa_170817.R
@@ -5,6 +5,7 @@ setwd("~/GRAD SCHOOL - CLEMSON/Project-Specific/R_Work")
 test <-read.csv("piwa_abund.csv")
 summary(test)
 str(test) #y.4 and Noise.4 and Wind.4 and Sky.4 JDate.4 are factors and shouldn't be
+#stringsAsFactors=FALSE, but this only works for reading, not for below.
 
 piwa.abund<- csvToUMF("piwa_abund.csv", long = FALSE, type = "unmarkedFramePCount")
     ##type may need to change for occupancy (occuRN, pcountOpen, or whichever used) ##
@@ -13,16 +14,19 @@ summary(piwa.abund)
 ?pcount
 
 null.piwa <- pcount(~1 ~1, piwa.abund)
-#global.piwa <- pcount(~ detection covariates 1 + 2 ~ covariates 1 + 2 + 3 + 4, piwa.abund)
-#local.piwa<-pcount(~ detection covariates 1 + 2 ~ covariates 2 + 3 + 4, piwa.abund)
-#lh.piwa<-pcount(~ det cov ~ covariates 3 + 4, piwa.abund)
-#landscape.piwa<-pcount(~ det cov ~ cov 5 + 6, piwa.abund)
-#treatment.piwa<-pcount(~ det cov ~ cov 7, piwa.abund)
+#global.piwa <- pcount(~ Noise + Wind + Sky + Jdate ~ Treatment + BA + Nsnags
+ #                     + Ccover + Ldepth + TreeHt + Age + TimeSinceB + Herbicide
+  #                    , piwa.abund)
+#local.piwa <- pcount(~ 1 ~ BA + Ccover + TreeHt + Ldepth, piwa.abund)
+#lh.piwa <- pcount(~ 1 ~ Ccover + TreeHt + BA, piwa.abund)
+##landscape.piwa <- pcount(~ 1 ~ cov 5 + 6, piwa.abund)
+#treatment.piwa <- pcount(~ 1 ~ Treatment + BA + TimeSinceB + Herbicide, piwa.abund)
 
-#fms <- fitList(names of all models)
-#ms1.cawr <- modSel(fms)
-#ms1.cawr@Full
-#ms1.cawr
+
+#fms <- fitList(null.piwa, global.piwa, local.piwa, lh.piwa, treatment.piwa)
+#ms1.piwa <- modSel(fms)
+#ms1.piwa@Full
+#ms1.piwa
 
 #see help for package "xlsReadWrite" in old notes, if need be#
-#write.table(ms1.cawr@Full, file="C:/Users/path.type",sep="\t")
+#write.table(ms1.piwa@Full, file="C:/Users/path.type",sep="\t")
